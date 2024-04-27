a65b18ae51b6ae48ed75249655507008e1cdb4af
Documenter.jl <documenter@juliadocs.github.io>
2024-04-27 01:53:41 +0000

    build based on de2d3d3

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestMacroExtensions","category":"page"},{"location":"#TestMacroExtensions","page":"Home","title":"TestMacroExtensions","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestMacroExtensions.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TestMacroExtensions]","category":"page"}]
+}
